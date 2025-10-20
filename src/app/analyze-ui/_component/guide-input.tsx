ea5bbd83d901d9ed45b3a190ab67db246bba3324
pyj <wodus331@gmail.com>
2025-10-20 17:36:52 +0900

    FloatingOutlinedInput 추가

--- src/app/analyze-ui/_component/guide-input.tsx
+++ src/app/analyze-ui/_component/guide-input.tsx
@@ -3,12 +3,14 @@ import Row from "@/components/Layout/Row";
 import Col from "@/components/Layout/Col";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
+import { FloatingOutlinedInput } from "@/components/FloatingOutlinedInput";
 
 export default function GuideInput() {
   const [input1, setInput1] = useState<string>("");
   const [input2, setInput2] = useState<string>("");
   const [input3, setInput3] = useState<string>("");
   const [input4, setInput4] = useState<string>("테스트");
+  const [input5, setInput5] = useState<string>("");
   return (
     <Col className={"gap-[30px] bg-background py-[24px]"}>
       <Row className={"flex-wrap items-center gap-[10px]"}>
@@ -107,6 +109,14 @@ export default function GuideInput() {
           value={input4}
           onChange={(e) => setInput4(e.target.value)}
           errorMessage={"input4 Error"}
+        />
+      </Row>
+      <Row className={"flex-wrap items-center gap-[10px]"}>
+        <FloatingOutlinedInput
+          id={"ddddddd"}
+          label={"테스트"}
+          value={input5}
+          onChangeValue={(value: string) => setInput5(value)}
         />
       </Row>
     </Col>
---
+++ src/components/FloatingOutlinedInput.tsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
+import Col from "@/components/Layout/Col";
+import Row from "@/components/Layout/Row";
+
+interface IFloatingOutlinedInputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  className?: string;
+  onChangeValue: (value: string) => void;
+  isError?: boolean;
+  id?: string;
+}
+
+export function FloatingOutlinedInput(props: IFloatingOutlinedInputProps) {
+  const { label, className, onChangeValue, isError, id, ...rest } = props;
+  const [isFocused, setIsFocused] = useState(false);
+
+  useEffect(() => {
+    if (rest.value) {
+      setIsFocused(true);
+    }
+  }, [rest.value]);
+
+  return (
+    <Col className={cn("relative isolate h-[56px] justify-center", className)}>
+      <input
+        id={id}
+        onFocus={() => setIsFocused(true)}
+        onBlur={(e) => {
+          if (!e.target.value) setIsFocused(false);
+        }}
+        onChange={(e) => onChangeValue(e.target.value)}
+        className={
+          "body3 m-[2px] flex h-[28px] flex-1 border-none bg-background px-[14px] py-[12px] focus-visible:outline-none"
+        }
+        {...rest}
+      />
+      <label
+        htmlFor={id}
+        className={cn(
+          "body3 absolute left-2 bg-background px-2",
+          isFocused
+            ? "left-1 translate-y-[-110%] scale-[0.8] text-[#0b57b0]"
+            : "",
+          isError && "text-[#b3261e]",
+        )}
+        style={{ transition: "all .3s cubic-bezier(0.4,0,0.2,1)" }}
+      >
+        {label}
+      </label>
+      <Row
+        className={cn(
+          "absolute z-[-1] h-full w-full rounded-[4px] border border-[#1f1f1f]",
+          isError && "border-[#b3261e]",
+        )}
+      />
+      <Row
+        className={cn(
+          "absolute z-[-1] h-full w-full rounded-[4px] border-[3px] border-[#0b57b0]",
+          isError && "border-[#b3261e]",
+        )}
+        style={{
+          transition: "opacity .3s cubic-bezier(0.4,0,0.2,1)",
+          opacity: isFocused ? "1" : "0",
+        }}
+      />
+    </Col>
+  );
+}
